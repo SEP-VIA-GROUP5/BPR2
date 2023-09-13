f024d9e807a31cbbd4813c11cbfc8206fb3e31cf
Rytis Ziaukas <71268611+rycka13@users.noreply.github.com>
2023-09-13 20:04:54 +0200

    Initial commit
    Created project for BE

---
+++ RentIT/src/main/java/com/rentit/rentit/RentItApplication.java
@@ -0,0 +1,13 @@
+package com.rentit.rentit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentItApplication.class, args);
+    }
+
+}
---
+++ RentIT/src/test/java/com/rentit/rentit/RentItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rentit.rentit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentItApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
