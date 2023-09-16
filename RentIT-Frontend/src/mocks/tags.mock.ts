e4b7ed2f56be9943d282218119e822796f8f758e
Ioan Sorin Baicoianu <baicoianuioansorin@gmail.com>
2023-09-16 18:10:31 +0200

    NO-30/NO-47/frontend/added mock-ups for model

---
+++ RentIT-Frontend/src/mocks/tags.mock.ts
@@ -0,0 +1,20 @@
+import {Tag} from "src/model/tag";
+
+export const mockedTags: Tag[] = [
+  {
+    tagId: 2331,
+    name: "Camera",
+  },
+  {
+    tagId: 1312,
+    name: "Car",
+  },
+  {
+    tagId: 123,
+    name: "Bike",
+  },
+  {
+    tagId: 123,
+    name: "Boat",
+  }
+]
