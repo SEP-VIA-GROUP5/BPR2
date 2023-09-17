b553fbcf05c212803f16fd382483bd0507ab425e
Titas212 <45366405+Titas212@users.noreply.github.com>
2023-09-17 14:39:01 +0200

    NO-60-backend-local-db-configs
    Added back @Mapper annotation.

--- RentIT/src/main/java/com/rentit/dao/interfaces/IProductMapper.java
+++ RentIT/src/main/java/com/rentit/dao/interfaces/IProductMapper.java
@@ -2,9 +2,10 @@ package com.rentit.dao.interfaces;
 
 
 import com.rentit.model.Product;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
-
+@Mapper
 public interface IProductMapper {
     List<Product> getNProductsByPage(int pageNum, int n);
 }
