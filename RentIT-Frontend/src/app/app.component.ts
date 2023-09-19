d6984513397ee91e8ff13eacfb18b49d71c8722a
ioanbaicoianu <ioan.baicoianu@se.com>
2023-09-19 12:49:37 +0200

    NO-30/NO-64/frontend/improve importing of ICONS

--- RentIT-Frontend/src/app/app.component.ts
+++ RentIT-Frontend/src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent {
 
   menuItems: NbMenuItem[] = GENERAL_MENU_ITEMS;
   sidebarVisible: boolean = true;
+  protected readonly ICONS = ICONS;
 
   constructor(
     private nbMenuService: NbMenuService,
@@ -22,10 +23,5 @@ export class AppComponent {
     this.sidebarService.toggle(this.sidebarVisible);
     this.sidebarVisible = !this.sidebarVisible;
     return false;
-  }
-
-  // enums
-  getIconsEnum() {
-    return ICONS;
   }
 }
