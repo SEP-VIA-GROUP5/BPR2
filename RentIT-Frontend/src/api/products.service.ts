a5aafd334f855079b6860bbad3d22ac65f45d30b
Ioan Sorin Baicoianu <baicoianuioansorin@gmail.com>
2023-09-19 22:19:58 +0200

    NO-30/NO-53/frontend/setup api fetch inside the products.state.ts

--- RentIT-Frontend/src/api/products.service.ts
+++ RentIT-Frontend/src/api/products.service.ts
@@ -2,6 +2,8 @@ import { Observable } from "rxjs";
 import { ApiService } from "src/core/services/api.service";
 import { Injectable } from "@angular/core";
 import { Product } from "src/model/product";
+import { environment } from "src/environments/environment.dev";
+import { mockedProducts } from "src/mocks/products.mock";
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +16,10 @@ export class ProductsService {
 
   PATH_CONTROLLER = 'product';
 
-  getMoviesPerPage(pageNumber, pageSize): Observable<Product[]> {
-    return this.apiService.get(`${this.PATH_CONTROLLER}/page/${pageNumber}/${pageSize}`);
+  getProductsPerPage(pageNumber, pageSize): Promise<Product[]> {
+    if(environment.mocked) {
+      return Promise.resolve(mockedProducts);
+    }
+    return this.apiService.get(`${this.PATH_CONTROLLER}/page/${pageNumber}/${pageSize}`).toPromise();
   }
 }
