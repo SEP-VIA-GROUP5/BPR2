7554eb128805efdf89d07517cc0780dd1843b380
Ioan Sorin Baicoianu <baicoianuioansorin@gmail.com>
2023-09-22 15:36:09 +0200

    NO-30/NO-59/frontend/moved app component html content inside the component.ts

--- RentIT-Frontend/src/app/app.component.ts
+++ RentIT-Frontend/src/app/app.component.ts
@@ -4,7 +4,36 @@ import {GENERAL_MENU_ITEMS, ICONS} from "src/app/constants";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+      <nb-layout>
+          <nb-layout-header>
+              <div class="left-section">
+                  <a href="#" (click)="toggleSidebar()">
+                      <nb-icon [icon]="ICONS.LIST_OUTLINE"></nb-icon>
+                      <i class="nb-menu"></i>
+                  </a>
+              </div>
+
+              <div class="center-section">
+                  <search-bar [icon]="ICONS.SEARCH"></search-bar>
+              </div>
+
+              <div class="right-section">
+                  <a href="">
+                      <img [src]="getImageBySize()" alt="Image" width="100%" height="auto"/>
+                  </a>
+              </div>
+          </nb-layout-header>
+          <nb-sidebar [responsive]="true">
+              <div>
+                  <nb-menu tag="menu" [items]="menuItems"></nb-menu>
+              </div>
+          </nb-sidebar>
+
+          <nb-layout-column class="colored-column-basic">
+              <router-outlet></router-outlet>
+          </nb-layout-column>
+      </nb-layout>`,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
