06f987a7f5514bf2040e30a88f47e65fc9dd7e0e
Baicoianu Ioan-Sorin <71268639+BaicoianuIoanSorin@users.noreply.github.com>
2023-10-01 18:39:41 +0200

    NO-74/NO-84/frontend/improved products api

--- RentIT-Frontend/src/api/products.service.ts
+++ RentIT-Frontend/src/api/products.service.ts
@@ -1,9 +1,8 @@
-import { Observable } from "rxjs";
 import { ApiService } from "src/core/services/api.service";
 import { Injectable } from "@angular/core";
 import { Product } from "src/model/product";
-import { environment } from "src/environments/environment.dev";
 import { mockedProducts } from "src/mocks/products.mock";
+import { simulateAPICall } from "src/constants";
 
 @Injectable({
   providedIn: 'root'
@@ -17,17 +16,7 @@ export class ProductsService {
   PATH_CONTROLLER = 'product';
 
   async getProductsPerPage(pageNumber, pageSize): Promise<Product[]> {
-    if (environment.mocked) {
-      await this.delayIfMocked();
-      return Promise.resolve(mockedProducts);
-    }
+    await simulateAPICall(mockedProducts);
     return this.apiService.get(`${this.PATH_CONTROLLER}/page/${pageNumber}/${pageSize}`).toPromise();
-  }
-
-  private async delayIfMocked() {
-    if(environment.mocked) {
-      const delay = ms => new Promise(res => setTimeout(res, ms));
-      await delay(2000);
-    }
   }
 }
