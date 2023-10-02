d591769bac85d710b2d394b36b07ddef82b7b674
Rytis Ziaukas <71268611+rycka13@users.noreply.github.com>
2023-10-02 20:18:46 +0200

    NO-88/backend/Can-now-get-user-object-from-token

--- RentIT/src/main/java/com/rentit/services/TokenService.java
+++ RentIT/src/main/java/com/rentit/services/TokenService.java
@@ -1,10 +1,11 @@
 package com.rentit.services;
 
-import org.springframework.security.oauth2.jwt.JwtClaimsSet;
-import org.springframework.security.oauth2.jwt.JwtEncoder;
-import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -17,25 +18,30 @@ import java.util.stream.Collectors;
 public class TokenService {
 
     private final JwtEncoder encoder;
+    private final JwtDecoder decoder;
     public static final int duration = 1; //in hours
 
-    public TokenService(JwtEncoder encoder) {
+    public TokenService(JwtEncoder encoder, JwtDecoder decoder) {
         this.encoder = encoder;
+        this.decoder = decoder;
     }
 
     public String generateToken(Authentication authentication) {
-        Instant now = Instant.now();
         String scope = authentication.getAuthorities().stream()
                 .map(GrantedAuthority::getAuthority)
                 .collect(Collectors.joining(" "));
         JwtClaimsSet claims = JwtClaimsSet.builder()
                 .issuer("self")
-                .issuedAt(now)
-                .expiresAt(now.plus(duration, ChronoUnit.HOURS))
+                .issuedAt(Instant.now())
+                .expiresAt(Instant.now().plus(duration, ChronoUnit.HOURS))
                 .subject(authentication.getName())
                 .claim("scope", scope)
                 .build();
         return this.encoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+    }
+
+    public String decodeToken(String tokenValue, String claim){
+        return this.decoder.decode(tokenValue).getClaimAsString(claim);
     }
 
 }
