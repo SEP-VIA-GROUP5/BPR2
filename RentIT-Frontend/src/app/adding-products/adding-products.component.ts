4ca59443bc4d26dfabe06cfd5b32d11dcf11215f
Ioan Sorin Baicoianu <baicoianuioansorin@gmail.com>
2023-10-06 12:25:40 +0200

    NO-68/NO-99/frontend/added adding products page initila

---
+++ RentIT-Frontend/src/app/adding-products/adding-products.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adding-products',
+  templateUrl: './adding-products.component.html',
+  styleUrls: ['./adding-products.component.scss']
+})
+export class AddingProductsComponent {
+
+}
