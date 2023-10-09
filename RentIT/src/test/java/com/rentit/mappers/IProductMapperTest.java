285773dd5b566802a46dee193a77b33dff9f994d
Titas212 <45366405+Titas212@users.noreply.github.com>
2023-10-09 20:04:14 +0200

    NO-109/backend/product-mapper-testing
    MyBatis product mapper testing. Test for adding a product.

--- RentIT/src/test/java/com/rentit/mappers/IProductMapperTest.java
+++ RentIT/src/test/java/com/rentit/mappers/IProductMapperTest.java
@@ -2,13 +2,15 @@ package com.rentit.mappers;
 
 import com.rentit.dao.interfaces.IProductMapper;
 import com.rentit.model.Product;
+import com.rentit.model.enums.ProductStatus;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(properties = "spring.config.name=application-test")
 public class IProductMapperTest {
@@ -21,5 +23,23 @@ public class IProductMapperTest {
         int n = 3;
         List<Product> products = productMapper.getNProductsByPage(1, n);
         assertThat(products.size()).isEqualTo(3);
+    }
+
+    @Test
+    public void add_product_successfully() {
+        Product product = Product.builder()
+                .name("test_product")
+                .dayPrice(10)
+                .weekPrice(10)
+                .monthPrice(10)
+                .city("test_city")
+                .category("test_category")
+                .status(ProductStatus.AVAILABLE)
+                .images(List.of())
+                .userId(1)
+                .build();
+        productMapper.addProduct(product);
+        List<Product> products = productMapper.getProductsByName("test_product");
+        assertThat(products.get(0)).isEqualTo(product);
     }
 }
