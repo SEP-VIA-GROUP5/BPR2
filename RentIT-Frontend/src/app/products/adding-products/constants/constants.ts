0ae6d0fcc71ddbd4881e6b1dddfe87eb1a992137
ioanbaicoianu <140078209+ioanbaicoianu@users.noreply.github.com>
2023-10-10 13:49:38 +0200

    NO-104/frontend/updated model

--- RentIT-Frontend/src/app/products/adding-products/constants/constants.ts
+++ RentIT-Frontend/src/app/products/adding-products/constants/constants.ts
@@ -1,7 +1,28 @@
+import {Product} from "src/model/product";
+
 export enum ADDING_PRODUCTS_STEP {
   PICTURES = 'Pictures',
+  DETAILS = 'Details',
 }
 
 export enum ADDING_PRODUCTS_TITLE {
   ADD_PICTURES = 'Add pictures',
+  ADD_DETAILS = 'Add details',
+}
+
+export const defaultProduct: Product = {
+  name: '',
+  description: '',
+  dayPrice: 0,
+  weekPrice: 0,
+  monthPrice: 0,
+  deposit: 0,
+  city: '',
+  productValue: 0,
+  minLeasePeriod: null,
+  category: '',
+  tag: [],
+  images: [],
+  productStatus: null,
+  rentedUntil: null
 }
