1b19c2e65f4a04aa6e949abada7fc440e7a97dbc
ioanbaicoianu <140078209+ioanbaicoianu@users.noreply.github.com>
2023-10-20 20:20:55 +0200

    NO-126/added navigation to product overview on click

--- RentIT-Frontend/src/app/shared-components/product-card/product-card.component.ts
+++ RentIT-Frontend/src/app/shared-components/product-card/product-card.component.ts
@@ -2,6 +2,7 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {Product} from "src/model/product";
 import {ICONS} from "src/app/constants";
 import {ProductSelected} from "src/app/shared-components/product-card/constants/constants";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'product',
@@ -17,15 +18,21 @@ export class ProductCardComponent {
 
   protected readonly ICONS = ICONS;
 
+  constructor(private router: Router) {
+  }
+
   // TODO product should be selected and the icon should be displayed with the fill out color
   // TODO should only select if an action is selected from the my-products page for example
-  selectProductAction(): void {
+  onClickProduct(): void {
     if (this.canPerformSelectProductAction) {
       this.isProductSelected = !this.isProductSelected;
       this.selectProduct.emit({
         isProductSelected: this.isProductSelected,
         product: this.product,
       } as ProductSelected);
+    }
+    else {
+      this.router.navigate([`/product/${this.product.id}`]);
     }
   }
 
