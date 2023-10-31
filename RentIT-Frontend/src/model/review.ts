084143ff99edca1baa33b9a6fe437a67bd1654f3
ioanbaicoianu <140078209+ioanbaicoianu@users.noreply.github.com>
2023-10-31 13:34:18 +0100

    NO-136/frontend/updated model

--- RentIT-Frontend/src/model/review.ts
+++ RentIT-Frontend/src/model/review.ts
@@ -2,9 +2,15 @@ import {User} from "src/model/user";
 
 export interface Review {
   // this is gathered when receiving data, not adding
+  // this can be userEmail or productId
   targetId?: string;
   rating: number;
   message: string;
   // TODO not implemented yet.
-  reviewer: User;
+  reviewer?: User;
+}
+
+export enum TARGET {
+  USER = 'user',
+  PRODUCT = 'product',
 }
