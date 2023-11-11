1b815fd1263cd2fa7c21931b4244693b40f1eb15
ioanbaicoianu <140078209+ioanbaicoianu@users.noreply.github.com>
2023-11-11 20:15:18 +0100

    NO-182/updated model for filtering

--- RentIT-Frontend/src/app/products/products/constants/constants.ts
+++ RentIT-Frontend/src/app/products/products/constants/constants.ts
@@ -13,22 +13,38 @@ export function mapToMapperFromFilteringOptions(filteringProductOptions: Filteri
     mapper.set('city', filteringProductOptions.city);
   }
   if (filteringProductOptions.depositFrom) {
-    mapper.set('deposit', filteringProductOptions.depositFrom.toString());
+    mapper.set('deposit_up', filteringProductOptions.depositFrom.toString());
+  }
+  if (filteringProductOptions.depositTo) {
+    mapper.set('deposit_down', filteringProductOptions.depositTo.toString());
   }
   if (filteringProductOptions.category) {
     mapper.set('category', filteringProductOptions.category);
   }
+  // TODO THESE DOES NOT WORK AT THE MOMENT
   if (filteringProductOptions.dayPriceFrom) {
-    mapper.set('day_price', filteringProductOptions.dayPriceFrom.toString());
+    mapper.set('day_price_up', filteringProductOptions.dayPriceFrom.toString());
+  }
+  if (filteringProductOptions.dayPriceTo) {
+    mapper.set('day_price_down', filteringProductOptions.dayPriceTo.toString());
   }
   if (filteringProductOptions.weekPriceFrom) {
-    mapper.set('week_price', filteringProductOptions.weekPriceFrom.toString());
+    mapper.set('week_price_up', filteringProductOptions.weekPriceFrom.toString());
+  }
+  if (filteringProductOptions.weekPriceTo) {
+    mapper.set('week_price_down', filteringProductOptions.weekPriceTo.toString());
   }
   if (filteringProductOptions.monthPriceFrom) {
-    mapper.set('month_price', filteringProductOptions.monthPriceFrom.toString());
+    mapper.set('month_price_up', filteringProductOptions.monthPriceFrom.toString());
+  }
+  if (filteringProductOptions.monthPriceTo) {
+    mapper.set('month_price_down', filteringProductOptions.monthPriceTo.toString());
   }
   if (filteringProductOptions.productValueFrom) {
-    mapper.set('product_value', filteringProductOptions.productValueFrom.toString());
+    mapper.set('product_value_up', filteringProductOptions.productValueFrom.toString());
+  }
+  if (filteringProductOptions.productValueTo) {
+    mapper.set('product_value_down', filteringProductOptions.productValueTo.toString());
   }
   return mapper;
 }
