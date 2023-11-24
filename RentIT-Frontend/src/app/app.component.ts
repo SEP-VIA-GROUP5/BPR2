6aa8c89b15c4ac4a1506a16eca50a7bb7091f093
ioanbaicoianu <140078209+ioanbaicoianu@users.noreply.github.com>
2023-11-24 18:34:08 +0100

    NO-192/frontend/Solved bug menu item selected does not update when navigating through logo to main page

--- RentIT-Frontend/src/app/app.component.ts
+++ RentIT-Frontend/src/app/app.component.ts
@@ -141,18 +141,18 @@ export class AppComponent implements OnInit, OnDestroy {
 
         // deselecting previous selected item
         if (this.currentSelectedItem) {
-         this.handleSelectionOfMenuItem(this.currentSelectedItem.tag, false, this.currentSelectedItem);
+         this.handleSelectionOfMenuItem(false, this.currentSelectedItem);
         }
 
         // selecting current selected item
-        this.handleSelectionOfMenuItem(item.tag, true, item)
+        this.handleSelectionOfMenuItem( true, item)
         this.currentSelectedItem = item;
       });
 
     this.store.dispatch([...actionsInParallel]);
   }
 
-  handleSelectionOfMenuItem(tag: string, selected: boolean, item: NbMenuBag) {
+  handleSelectionOfMenuItem(selected: boolean, item: NbMenuBag) {
     let indexOfCurrentSelectedItem;
     if (item.tag === TAG_MENU_ITEMS.USER_MENU) {
       indexOfCurrentSelectedItem = this.userSidebarMenuItems.findIndex(menuItem => menuItem.link === item.item.link);
@@ -211,14 +211,22 @@ export class AppComponent implements OnInit, OnDestroy {
 
   navigateToMainPage() {
     this.router.navigate([PRODUCTS_MENU_ITEM_URLS.PRODUCTS]);
+    if (this.currentSelectedItem) {
+      this.handleSelectionOfMenuItem(false, this.currentSelectedItem);
+    }
+    // find the menu item for products
+    let indexOfProductsMenuItem = this.generalSidebarMenuItems.findIndex(menuItem => menuItem.link === PRODUCTS_MENU_ITEM_URLS.PRODUCTS);
+    let item = {
+      tag: TAG_MENU_ITEMS.GENERAL_MENU,
+      item: this.generalSidebarMenuItems[indexOfProductsMenuItem]
+    } satisfies NbMenuBag;
+    this.handleSelectionOfMenuItem( true, item)
+    this.currentSelectedItem = item;
+
   }
 
   isOnMainPage(): boolean {
     return this.router.routerState.snapshot.url === PRODUCTS_MENU_ITEM_URLS.PRODUCTS;
-  }
-
-  onContextChange(event) {
-    console.log(event );
   }
 
   ngOnDestroy() {
