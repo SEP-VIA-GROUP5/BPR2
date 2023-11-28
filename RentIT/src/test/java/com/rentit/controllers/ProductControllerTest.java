59946213962d5ea8ea1e40dc5512e74187ae03a9
Rytis Ziaukas <71268611+rycka13@users.noreply.github.com>
2023-11-28 21:38:29 +0100

    Fixed broken tests and added more coverage

--- RentIT/src/test/java/com/rentit/controllers/ProductControllerTest.java
+++ RentIT/src/test/java/com/rentit/controllers/ProductControllerTest.java
@@ -3,6 +3,8 @@ package com.rentit.controllers;
 import com.rentit.model.Image;
 import com.rentit.model.Product;
 import com.rentit.model.dto.ProductDTO;
+import com.rentit.model.dto.ProductPackageDTO;
+import com.rentit.model.dto.UserDTO;
 import com.rentit.model.enums.ProductStatus;
 import com.rentit.services.ProductService;
 import com.rentit.services.enums.ResponseMessage;
@@ -21,6 +23,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import static org.hamcrest.Matchers.emptyOrNullString;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -40,6 +43,7 @@ public class ProductControllerTest {
     private List<Image> images;
     private Product product;
     private ProductDTO productDTO;
+    private UserDTO userDTO;
     @BeforeEach
     void beforeEach(){
         images = new ArrayList<>();
@@ -78,6 +82,13 @@ public class ProductControllerTest {
                 .images(images)
                 .status(ProductStatus.AVAILABLE)
                 .rentedUntil(LocalDate.parse("2024-05-01"))
+                .build();
+        userDTO = UserDTO.builder()
+                .email("testemail@gmail.com")
+                .firstName("Test name")
+                .lastName("Test lastname")
+                .location("Test city")
+                .phoneNumber("123")
                 .build();
     }
 
@@ -141,15 +152,62 @@ public class ProductControllerTest {
                         .header("Authorization","test"))
                 .andDo(print())
                 .andExpect(status().isBadRequest());
-        when(service.deleteProductById(-1,"test")).thenReturn(ResponseMessage.DELETION_ERROR);
+        when(service.deleteProductById(-1,"test")).thenReturn(ResponseMessage.INVALID_PARAMETERS);
         this.mvc.perform(delete("http://localhost:8080/product/id/-1")
                         .header("Authorization","test"))
                 .andDo(print())
                 .andExpect(status().isNotFound());
-        when(service.deleteProductById(1,"test")).thenReturn(ResponseMessage.INTERNAL_ERROR);
+        when(service.deleteProductById(1,"test")).thenReturn(ResponseMessage.CREDENTIALS_ERROR);
         this.mvc.perform(delete("http://localhost:8080/product/id/1")
                         .header("Authorization","test"))
                 .andDo(print())
-                .andExpect(status().isInternalServerError());
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    public void testGetProductById() throws Exception {
+        when(service.getProductById(1)).thenReturn(new ProductPackageDTO(productDTO, userDTO));
+        this.mvc.perform(get("http://localhost:8080/product/id/1")).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().json("{\"product\":{\"id\":1,\"name\":\"Test name\",\"description\":\"Test description\",\"dayPrice\":5.0,\"weekPrice\":10.0,\"monthPrice\":100.0,\"deposit\":10.0,\"city\":\"Test city\",\"productValue\":100.0,\"minLeasePeriod\":1,\"category\":\"test category\",\"tags\":[\"test tag\",\"test tag2\"],\"images\":[{\"id\":1,\"imageUrl\":\"http://test.url\"}],\"status\":\"AVAILABLE\",\"rentedUntil\":\"2024-05-01\"},\"user\":{\"email\":\"testemail@gmail.com\",\"firstName\":\"Test name\",\"lastName\":\"Test lastname\",\"location\":\"Test city\",\"phoneNumber\":\"123\"}}"));
+    }
+
+    @Test
+    public void testGetProductByIdErrors() throws Exception {
+        when(service.getProductById(-1)).thenReturn(null);
+        this.mvc.perform(get("http://localhost:8080/product/id/-1")).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(emptyOrNullString()));
+    }
+
+    @Test
+    public void testGetPageOfFilteredProducts() throws Exception {
+        when(service.getNProductsByPageWithFilters(1, 1, Collections.emptyMap())).thenReturn(Collections.singletonList(productDTO));
+        this.mvc.perform(get("http://localhost:8080/product/page/1/1/filter")).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().json("[{\"id\":1,\"name\":\"Test name\",\"description\":\"Test description\",\"dayPrice\":5.0,\"weekPrice\":10.0,\"monthPrice\":100.0,\"deposit\":10.0,\"city\":\"Test city\",\"productValue\":100.0,\"minLeasePeriod\":1,\"category\":\"test category\",\"tags\":[\"test tag\",\"test tag2\"],\"images\":[{\"id\":1,\"imageUrl\":\"http://test.url\"}],\"status\":\"AVAILABLE\",\"rentedUntil\":\"2024-05-01\"}]"));
+    }
+
+    @Test
+    public void testGetMyList() throws Exception {
+        when(service.getMyList("test")).thenReturn(Collections.singletonList(productDTO));
+        this.mvc.perform(get("http://localhost:8080/product/myList").header("Authorization", "test")).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().json("[{\"id\":1,\"name\":\"Test name\",\"description\":\"Test description\",\"dayPrice\":5.0,\"weekPrice\":10.0,\"monthPrice\":100.0,\"deposit\":10.0,\"city\":\"Test city\",\"productValue\":100.0,\"minLeasePeriod\":1,\"category\":\"test category\",\"tags\":[\"test tag\",\"test tag2\"],\"images\":[{\"id\":1,\"imageUrl\":\"http://test.url\"}],\"status\":\"AVAILABLE\",\"rentedUntil\":\"2024-05-01\"}]"));
+    }
+
+    @Test
+    public void testGetUserProductList() throws Exception {
+        when(service.getUserProductList("test@example.com")).thenReturn(Collections.singletonList(productDTO));
+        this.mvc.perform(get("http://localhost:8080/product/productList/test@example.com")).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().json("[{\"id\":1,\"name\":\"Test name\",\"description\":\"Test description\",\"dayPrice\":5.0,\"weekPrice\":10.0,\"monthPrice\":100.0,\"deposit\":10.0,\"city\":\"Test city\",\"productValue\":100.0,\"minLeasePeriod\":1,\"category\":\"test category\",\"tags\":[\"test tag\",\"test tag2\"],\"images\":[{\"id\":1,\"imageUrl\":\"http://test.url\"}],\"status\":\"AVAILABLE\",\"rentedUntil\":\"2024-05-01\"}]"));
+    }
+
+    @Test
+    public void testSetProductStatus() throws Exception {
+        when(service.setProductStatus(1, "status", "test", null)).thenReturn(ResponseMessage.SUCCESS);
+        this.mvc.perform(post("http://localhost:8080/product/status/1/status").header("Authorization", "test")).andDo(print())
+                .andExpect(status().isOk());
     }
 }
