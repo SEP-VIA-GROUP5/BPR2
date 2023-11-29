c98c7654dd7342303315b7dc31062b3956ba9160
Rytis Ziaukas <71268611+rycka13@users.noreply.github.com>
2023-11-29 18:53:11 +0100

    Fixed edit user

--- RentIT/src/main/java/com/rentit/dao/interfaces/IUserMapper.java
+++ RentIT/src/main/java/com/rentit/dao/interfaces/IUserMapper.java
@@ -7,6 +7,7 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface IUserMapper {
     User getUserByEmail(String email);
+    User getUserToEditById(int id);
     User getUserById(int id);
     UserDTO getUserDTOById(int id);
     void registerUser(User user);
