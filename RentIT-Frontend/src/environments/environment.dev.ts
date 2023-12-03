334440efeb635d102db7e3ca9b8ffce9331188b3
ioanbaicoianu <140078209+ioanbaicoianu@users.noreply.github.com>
2023-12-03 22:41:38 +0100

    Added back the api url for dev environment

--- RentIT-Frontend/src/environments/environment.dev.ts
+++ RentIT-Frontend/src/environments/environment.dev.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   mocked: false,
   test: false,
-  api_url: 'https://digital-yeti-406718-lq2q3x3hfa-uc.a.run.app/',
+  api_url: 'http://localhost:8080/',
   google_api_key: 'AIzaSyAGjAis9P3H5OB7bnPzZh5l-_FrgaOx-cE',
   imgur_api_client_id: '64373a7ddae18ac',
 }
