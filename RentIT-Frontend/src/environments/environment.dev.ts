471da100c23519eb0ea64d6a32c886cfb9701b27
ioanbaicoianu <140078209+ioanbaicoianu@users.noreply.github.com>
2023-12-06 13:42:42 +0100

    NO-26/frontend/small improvement - added disable button on review button, if the product is current user's product or user is not logged in

--- RentIT-Frontend/src/environments/environment.dev.ts
+++ RentIT-Frontend/src/environments/environment.dev.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   mocked: false,
   test: false,
-  api_url: 'http://localhost:8080/',
+  api_url: 'https://digital-yeti-406718-lq2q3x3hfa-uc.a.run.app/',
   google_api_key: 'AIzaSyAGjAis9P3H5OB7bnPzZh5l-_FrgaOx-cE',
   imgur_api_client_id: '64373a7ddae18ac',
 }
